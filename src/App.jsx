3d3a7b1847fcbf1985cfe07c3b30367766339054
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-29 14:06:51 +0200

    add a useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import { useState } from "react";
+import { ColorForm } from "./Components/ColorForm/ColorForm";
 import "./App.css";
 
 function App() {
+  // call a useState hook and define 'initialColors' as the initial value
+  // set 'colors' as current state and 'setColors' as set function inside the variable
+  const [colors, setColors] = useState(initialColors);
+
   return (
     <>
       <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
+      <ColorForm />
+
+      {/* swap 'initialColors.map' for 'colors.map' so the UI always reflects the latest state */}
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
